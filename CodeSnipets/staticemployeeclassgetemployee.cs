404dd072d0bc22b6c73e246ecf64a2790d093367
chintitomasud3 <45783647+chintitomasud3@users.noreply.github.com>
2022-11-06 11:10:09 +0600

    Create staticemployeeclassgetemployee.cs

---
+++ CodeSnipets/staticemployeeclassgetemployee.cs
@@ -0,0 +1,18 @@
+public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<Employee> GetEmployee() {
+
+            List<Employee> employees = new List<Employee>()
+            {
+               new Employee(){ Id=1,Name="Masudur rahman"},
+               new Employee(){ Id=2,Name="sumaiya mehreen meem"}
+            };
+
+            return employees;    
+         
+         }
+            
+        }
