a190411054e3fdde8adc475abf0534d36a18ac0e
chintitomasud3 <45783647+chintitomasud3@users.noreply.github.com>
2022-11-06 11:36:23 +0600

    Update staticemployeeclassgetemployee.cs

--- CodeSnipets/staticemployeeclassgetemployee.cs
+++ CodeSnipets/staticemployeeclassgetemployee.cs
@@ -16,4 +16,74 @@ public class Employee
          }
             
         }
-.
+
+
+
+public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+
+        public static List<Department> GetDepartments() {
+
+            List<Department> departments = new List<Department>()
+            {
+                new Department{ DepartmentId=1,Name="HR"},
+                new Department{ DepartmentId=2,Name="IT"}
+
+            };
+            return departments;
+        
+        }
+    }
+
+
+
+
+
+public IActionResult Index()
+
+        {
+            // IEnumerable<Employee> employees = Employee.GetEmployee().Where(emp=>emp.DepartmentId==2);
+            var result = from e in Employee.GetEmployee()
+                         join d in Department.GetDepartments()
+                         on e.DepartmentId equals d.DepartmentId
+                         select new Customviewmodel
+                         {
+                             employeename = e.Name,
+                             departmentname = d.Name
+                         };
+            
+            
+            return View(result);
+        }
+
+
+
+
+public class Customviewmodel
+    {
+        public string employeename { get; set; }
+        public string departmentname { get; set; }
+    }
+
+
+
+
+
+
+@model IEnumerable<freecode.Models.Customviewmodel>
+@{
+    ViewData["Title"] = "Home Page";
+}
+
+<div class="text-center">
+    <h1 class="display-4">Welcome</h1>
+    <p>Learn about <a href="https://docs.microsoft.com/aspnet/core">building Web apps with ASP.NET Core</a>.</p>
+</div>
+
+@foreach(var item in @Model){
+    <h1>ID:@item.employeename</h1>
+    <h2>Name: @item.departmentname</h2>
+     
+}
